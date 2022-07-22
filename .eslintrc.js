a09ec87458d50e0ed4e90e718ef35fdb5cdd4ea8
Ramon Trepat <r.trepis@gmail.com>
2022-07-22 19:59:19 +0200

    remove eslint extens aireByAndBy-typeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+  extends: ["airbnb-base", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
