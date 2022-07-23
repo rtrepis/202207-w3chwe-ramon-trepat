e1bf620f5e78cb7f47a924dd35ed3ac2feaa7d3a
Ramon Trepat <r.trepis@gmail.com>
2022-07-23 07:47:37 +0200

    add types i-components and element Component

---
+++ public/js/index.js
@@ -0,0 +1,2 @@
+import Component from "./components/Component/Component.js";
+new Component(document.body, "pokemons", "div");
---
+++ src/ts/components/Component/Component.ts
@@ -0,0 +1,14 @@
+import IComponent from "../../types/i-components.js";
+
+class Component implements IComponent {
+  element: HTMLElement;
+
+  constructor(parent: HTMLElement, className: string, tag = "div") {
+    this.element = document.createElement(tag);
+    this.element.className = className;
+
+    parent.appendChild(this.element);
+  }
+}
+
+export default Component;
