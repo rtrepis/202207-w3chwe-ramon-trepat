c92f3cf500498729d2c9a2cd93c64a04248ef69f
Ramon Trepat <r.trepis@gmail.com>
2022-07-23 07:45:40 +0200

    add configs tsconfigs and eslint and eslint-config-airbnb-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
